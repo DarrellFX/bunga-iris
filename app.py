c187a6fcafe7a1500306730dd88ec7628c626e71
DarrellFX <88232610+DarrellFX@users.noreply.github.com>
2025-12-01 16:22:10 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -13,20 +13,23 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.text("Sepal characteristics")
-    sepal_1 = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
-    sepal_w - st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
+    sepal_l = st.slider('Sepal length (cm)', 1.0, 8.0, 0.5)
+    # PERBAIKAN 1: Mengganti '-' menjadi '='
+    sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
 
-    with col2:
-        st.text("Pepal characteristics")
-        petal_1 = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
-        petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
+# PERBAIKAN 2: Menggeser 'with col2' ke kiri (un-indent) agar sejajar dengan col1
+with col2:
+    st.text("Petal characteristics")
+    petal_l = st.slider('Petal length (cm)', 1.0, 7.0, 0.5)
+    petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
 
-        st.text('')
-        if st.button("Predict type of Iris"):
-            result = predict(
-                np.array([[sepal_1, sepal_w, petal_w]]))
-            st.text(result[0])
+st.text('')
+if st.button("Predict type of Iris"):
+    # PERBAIKAN 3: Menambahkan variable 'petal_l' ke dalam array prediksi
+    # Urutan harus benar: sepal length, sepal width, petal length, petal width
+    result = predict(
+        np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
+    st.text(result[0])
 
 st.text('')
 st.text('')
-st.markdown()
